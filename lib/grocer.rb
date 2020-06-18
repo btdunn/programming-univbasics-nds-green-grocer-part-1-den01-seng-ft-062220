6bf4813eab54e5cb027011ede1a56e30d4e47e01
interactive-directory-1839 <benthedunn@gmail.com>
2020-06-18 17:54:39 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -13,6 +13,21 @@ end
 
 def consolidate_cart(cart)
 
+cart_update = []
+counter = 0 
 
-
+while counter < cart.length
+  new_item = find_item_by_name_in_collection(cart[counter][:item], cart_update)
+  if new_item != nil 
+    new_item[:count] += 1 
+  else
+  new_item = {
+    :item => cart[counter][:item],
+    :price => cart[counter][:price]
+    :clearance => cart[counter][:clearance]
+    :count => 1 
+  }
+  end
+  counter += 1 
+  end
 end
