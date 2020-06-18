173ac6a5404af0998b96a08ee7e583227e25b81f
interactive-directory-1839 <benthedunn@gmail.com>
2020-06-18 17:22:27 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,5 +1,3 @@
-
-
 def find_item_by_name_in_collection(name, collection)
 
 
@@ -8,9 +6,6 @@ end
     
 
 def consolidate_cart(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This returns a new Array that represents the cart. Don't merely
-  # change `cart` (i.e. mutate) it. It's easier to return a new thing.
+
 
 end
